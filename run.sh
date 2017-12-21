3c82cc8d85f949e8de9885405b16c71fa784f18a
duvallj <jediguy9@gmail.com>
2017-12-21 18:40:18 +0000

    Fix ipv6 bug?

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd /web/activities/othello
 echo "CHDIRED" > z.log
 source venv/bin/activate
 echo "SOURCED" >> z.log
-python3 flask_server.py --port $PORT --remotes ovm1.vm.sites.tjhsst.edu:45310 &> temp.log
+python3 flask_server.py --port $PORT --remotes [2001:468:cc0:1600:0:bff:9951:ac5b]=45310 &> temp.log
 echo "ENDED" >> z.log
