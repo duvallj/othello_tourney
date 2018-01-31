deb0850cca31fa72c1661d54e569e1942116b230
othellovm <root@othello.csl.tjhsst.edu>
2018-01-31 13:02:11 -0500

    save players, remove students folder

--- static/js/get_config.js
+++ static/js/get_config.js
@@ -21,15 +21,35 @@ function putConfigToPage(output){
   output += '\nYourself'
   pdict = output.split('\n');
   //console.log(output);
+  var player1 = me;
+  if(window.localStorage.getItem("player1")){
+      player1 = window.localStorage.getItem("player1");
+  }
+  var player2 = "";
+  if(window.localStorage.getItem("player2")){
+      player2 = window.localStorage.getItem("player2");
+  }
+  // console.log(player1);
+  // console.log(player2);
   buf = 'Black: <select id="ai1" placeholder="Player 1">';
   buf += '<option value="">Player 1</option>'
   for (var i=0; i<pdict.length; i++){
-    buf += '<option value="'+pdict[i]+'">'+pdict[i]+'</option>'
+    if(pdict[i] == player1){
+        buf += '<option value="'+pdict[i]+'" selected>'+pdict[i]+'</option>'
+    }
+    else {
+        buf += '<option value="'+pdict[i]+'">'+pdict[i]+'</option>'
+    }
   }
   buf += '</select><br><br>White: <select id="ai2" placeholder="Player 2">';
   buf += '<option value="">Player 2</option>'
   for (var i=0; i<pdict.length; i++){
-    buf += '<option value="'+pdict[i]+'">'+pdict[i]+'</option>'
+    if(pdict[i] == player2){
+        buf += '<option value="'+pdict[i]+'" selected>'+pdict[i]+'</option>'
+    }
+    else {
+        buf += '<option value="'+pdict[i]+'">'+pdict[i]+'</option>'
+    }
   }
   buf += '</select><br><br>';
   buf += 'Time limit (secs): <input type="number" id="tml" value="5"/><br><br>'
@@ -42,6 +62,8 @@ function putConfigToPage(output){
 function actuallyDoSocket(){
   var port1 = document.getElementById('ai1').value;
   var port2 = document.getElementById('ai2').value;
+  window.localStorage.setItem("player1", port1);
+  window.localStorage.setItem("player2", port2);
   var timelimit = document.getElementById('tml').value;
   makeSocketFromPage(HOST,PORT,port1,port2,'1000',timelimit,false);
 };
