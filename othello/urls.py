832042d0fd92fa372257f10df81af392d69cedc1
duvallj <2019jduvall@tjhsst.edu>
2018-02-21 14:18:53 -0500

    Add initial app structure
    Still need to put in urls/views/whatnot, but its a start

--- othello/urls.py
+++ othello/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+
+from 
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('', include('social_django.urls', namespace='social'),
+    url(r'^admin/', admin.site.urls),
 ]
