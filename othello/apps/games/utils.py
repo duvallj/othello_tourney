d68ce750e41059b87550ba646fbd22db74b1467c
Theo Ouzhinski <touzhinski@gmail.com>
2019-01-17 18:14:58 -0500

    Use django filtering

--- othello/apps/games/utils.py
+++ othello/apps/games/utils.py
@@ -9,6 +9,7 @@ def gen_room_id():
     """
     return get_random_string(length=settings.OTHELLO_ROOM_ID_LEN)
 
+
 def make_new_room():
     """
     Makes a new room. Usually called when a client connects via websocket
@@ -20,13 +21,22 @@ def make_new_room():
     while Room.objects.filter(room_id=room_id).exists(): room_id = gen_room_id()
     room, _ = Room.objects.get_or_create(room_id=room_id)
     return room
+
     
 def get_all_rooms():
     """
     Returns a list of all available rooms
     """
     return Room.objects.all()
+
     
+def get_playing_rooms():
+    """
+    Returns a list of all current playing games
+    """
+    return Room.objects.filter(playing=True)
+
+
 def delete_room(room_id):
     """
     Deletes a room. Usually called when client disconnects their websocket
--- othello/apps/games/views.py
+++ othello/apps/games/views.py
@@ -1,22 +1,27 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 
-from .utils import get_all_rooms
+from .utils import get_playing_rooms
 
-import os
 
 def play_view(request):
     return render(request, "play.html")
 
+
 def watch_view(request):
     return render(request, "watch.html")
+
 
 def about_view(request):
     return render(request, "about.html")
 
+
 def about_uploading_view(request):
     return render(request, "about_uploading.html")
+
 
 def ai_list_view(request):
     student_folder = settings.MEDIA_ROOT
@@ -28,8 +33,9 @@ def ai_list_view(request):
     possible_names = sorted(possible_names)
     return HttpResponse("\n".join(possible_names), content_type="text/plain")
 
+
 def game_list_view(request):
-    rooms = get_all_rooms()
+    rooms = get_playing_rooms()
     data_list = [
         "{},{},{},{}".format(
             room.room_id,
@@ -37,6 +43,6 @@ def game_list_view(request):
             room.white,
             room.timelimit
         )
-        for room in rooms if room.playing
+        for room in rooms
     ]
     return HttpResponse("\n".join(data_list), content_type="text/plain")
