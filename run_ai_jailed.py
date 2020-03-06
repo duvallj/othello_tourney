91ac3e81fa95a11d5b418e3f4c3fb68ac80761a6
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-03-06 09:39:54 -0500

    Add logging

--- run_ai_jailed.py
+++ run_ai_jailed.py
@@ -1,8 +1,19 @@
 import os, sys
-import logging as log
+import logging
+import time
+
 
 from othello.gamescheduler.run_ai_utils import JailedRunner
+from othello.gamescheduler.settings import OTHELLO_STUDENT_PATH
+
+log = logging.getLogger(__name__)
 
 if __name__=="__main__":
     log.basicConfig(format='%(asctime)s:%(levelname)s:[JAILED]:%(message)s', level=log.WARN)
+
+    handler = logging.FileHandler(os.path.join(OTHELLO_STUDENT_PATH, time.strftime("log-%Y-%m-%d-%H-%M-%S.log")))
+    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:[JAILED]:%(message)s"))
+    log.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
     JailedRunner(sys.argv[-1]).run()
