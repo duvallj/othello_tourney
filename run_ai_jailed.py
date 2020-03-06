9d0fd2cd45bf2c39d1535482c9821c3ea5b5fefa
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-03-06 09:47:22 -0500

    Set log level to debug

--- run_ai_jailed.py
+++ run_ai_jailed.py
@@ -9,11 +9,11 @@ from othello.gamescheduler.settings import OTHELLO_STUDENT_PATH
 log = logging.getLogger(__name__)
 
 if __name__=="__main__":
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:[JAILED]:%(message)s', level=logging.WARN)
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:[JAILED]:%(message)s', level=logging.DEBUG)
 
     handler = logging.FileHandler(os.path.join(OTHELLO_STUDENT_PATH, sys.argv[-1], time.strftime("log-%Y-%m-%d-%H-%M-%S.log")))
     handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:[JAILED]:%(message)s"))
     log.addHandler(handler)
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.DEBUG)
 
     JailedRunner(sys.argv[-1]).run()
