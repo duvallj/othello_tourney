8b1396305872af71501303547f1440384805de4b
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-03-06 09:55:09 -0500

    Completely rework logging setup

--- run_ai_jailed.py
+++ run_ai_jailed.py
@@ -9,11 +9,21 @@ from othello.gamescheduler.settings import OTHELLO_STUDENT_PATH
 log = logging.getLogger(__name__)
 
 if __name__=="__main__":
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:[JAILED]:%(message)s', level=logging.DEBUG)
+    root_logger = logging.getLogger(None)
+    root_logger.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s:[JAILED]:%(message)s")
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.WARN)
+    root_logger.addHandler(stream_handler)
 
     handler = logging.FileHandler(os.path.join(OTHELLO_STUDENT_PATH, sys.argv[-1], time.strftime("log-%Y-%m-%d-%H-%M-%S.log")))
-    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:[JAILED]:%(message)s"))
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
     log.addHandler(handler)
-    log.setLevel(logging.DEBUG)
+    root_logger.addHandler(handler)
 
     JailedRunner(sys.argv[-1]).run()
